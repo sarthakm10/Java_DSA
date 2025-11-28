6333a95b23c171c19a3324b55210afbe64f5e7d7
sarthakm10 <mishrasarthak33@gmail.com>
2025-11-28 23:44:04 +0530

    added some examples

---
+++ src/com/sarthak/Basics.java
@@ -0,0 +1,22 @@
+package com.sarthak;
+
+public class Basics {
+    public static void main(String[] args) {
+        int a = 10;
+        if (a == 10) {
+            System.out.println("Hello World");
+        }
+
+        // while loop
+        int count = 1;
+        while(count != 5) {
+            System.out.println(count);
+            count++;
+        }
+
+        // for loop
+        for(int num = 1; num != 5; num++) {
+            System.out.println(num);
+        }
+    }
+}
