d189b7c01cf6e7310388f3287c00ce487b99f41c
sarthakm10 <mishrasarthak33@gmail.com>
2026-01-17 23:32:15 +0530

    added Binary Search Examples

--- src/com/sarthak7/Floor.java
+++ src/com/sarthak7/Floor.java
@@ -1,18 +1,18 @@
 package com.sarthak7;
 
-public class BinarySearching {
+public class Floor {
     public static void main(String[] args) {
-        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
-        int target = 22;
-        int ans = binarySearch(arr, target);
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 1;
+        int ans = floor(arr, target);
         System.out.println(ans);
     }
-    // return the index
-    // return -1 if it does not exist
-    static int binarySearch(int arr[], int target){
+
+    // return the index: greatest number <= target
+    static int floor(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
-        while(start <= end){
+        while(start <= end) {
             int mid = start + (end - start) / 2;
             if (target < arr[mid]) {
                 end = mid - 1;
@@ -22,6 +22,6 @@ public class BinarySearching {
                 return mid;
             }
         }
-        return -1;
+        return end;
     }
 }
--- src/com/sarthak7/OrderAgnosticBS.java
+++ src/com/sarthak7/OrderAgnosticBS.java
@@ -17,21 +17,19 @@ public class OrderAgnosticBS {
 
         while(start <= end) {
             int mid = start + (end - start) / 2;
-
-            if (arr[mid] == target) {
+            if(arr[mid] == target) {
                 return mid;
             }
-
             if (isAsc) {
                 if (target < arr[mid]) {
                     end = mid - 1;
-                } else {
+                }else {
                     start = mid + 1;
                 }
-            } else {
-                if (target > arr[mid]) {
+            }else {
+                if(target > arr[mid]) {
                     end = mid - 1;
-                } else {
+                }else {
                     start = mid + 1;
                 }
             }
