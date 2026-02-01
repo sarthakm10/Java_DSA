844ec1c2952dd2436276dcc27dbdf71178fd2d8e
sarthakm10 <mishrasarthak33@gmail.com>
2026-02-01 15:49:39 +0530

    added cycle sorting

---
+++ src/com/sarthak08/CycleSorting.java
@@ -0,0 +1,25 @@
+package com.sarthak08;
+
+import java.util.Arrays;
+
+public class CycleSorting {
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 2, 3};
+        cycle(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    static void cycle(int arr[]){
+        int i = 0;
+        while(i < arr.length){
+            int correct_idx = arr[i] - 1;
+            if(arr[i] != arr[correct_idx]){
+                swap(arr, i, correct_idx);
+            }else   i++;
+        }
+    }
+}
