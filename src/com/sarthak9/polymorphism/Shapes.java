588c7378a1dfd64ea0929dddfc26e725963be0d8
sarthakm10 <mishrasarthak33@gmail.com>
2026-02-06 11:34:16 +0530

    added polymorphism

---
+++ src/com/sarthak9/polymorphism/Shapes.java
@@ -0,0 +1,7 @@
+package com.sarthak9.polymorphism;
+
+public class Shapes {
+    void area() {
+        System.out.println("I am in shapes");
+    }
+}
---
+++ src/com/sarthak9/polymorphism/Square.java
@@ -0,0 +1,7 @@
+package com.sarthak9.polymorphism;
+
+public class Square extends Shapes{
+    void area() {
+        System.out.println("Area is square of side");
+    }
+}
